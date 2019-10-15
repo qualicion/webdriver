8328b036282ad00bfc232954f124d6f7ea5a7ac1
toonday <qualicion@outlook.com>
2019-10-15 22:14:35 +0100

    committed first tests

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,34 @@
+package base;
+
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
+
+
+public class BaseTests {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(375,812));
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/dropdown/DropdownTest.java
@@ -0,0 +1,17 @@
+package dropdown;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class DropdownTest extends BaseTests {
+
+    @Test
+    public void testSelectOption(){
+        var dropDownPage = homePage.clickDropDown();
+
+
+
+
+    }
+
+}
