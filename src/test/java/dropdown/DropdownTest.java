9cae81c3a5924b787de48905e567e3e1095a89d2
toonday <qualicion@outlook.com>
2019-10-16 20:07:01 +0100

    added all new files for automation

--- src/test/java/dropdown/DropdownTest.java
+++ src/test/java/dropdown/DropdownTest.java
@@ -3,11 +3,20 @@ package dropdown;
 import base.BaseTests;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class DropdownTest extends BaseTests {
 
     @Test
     public void testSelectOption(){
         var dropDownPage = homePage.clickDropDown();
+
+        var option = "Option 1";
+        dropDownPage.selectFromDropDown(option);
+        var selectedOptions = dropDownPage.getSelectOptions();
+        assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
+        assertTrue(selectedOptions.contains(option), "Option not selected");
 
     }
 
