0b111dd4b7b2d91fe66f49c334b81d4f361ca656
toonday <qualicion@outlook.com>
2019-10-23 22:53:35 +0100

    Added hover test

--- src/main/java/pages/HoverPage.java
+++ src/main/java/pages/HoverPage.java
@@ -33,5 +33,20 @@ public class HoverPage {
             this.caption = caption;
         }
 
+        public boolean isCaptionDisplayed(){
+            return caption.isDisplayed();
+        }
+
+        public String getTitle(){
+            return caption.findElement(header).getText();
+        }
+
+        public String getLink(){
+            return caption.findElement(link).getAttribute("href");
+        }
+
+        public String getLinkText(){
+            return caption.findElement(link).getText();
+        }
     }
 }
